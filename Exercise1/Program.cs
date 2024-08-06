f7697a2f3a9e2ba75061f4e0b52a26bdc426d80d
NethanelGuetta <netinello@gmail.com>
2024-08-06 09:38:17 +0300

    Add project files.

---
+++ Exercise1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+public class Person()
+{ 
+    private int Age { get; set; }
+    public void SetAge(int age)
+    { 
+        Person person = new Person();
+        person.Age = age;
+    }
+
+    public int GetAge(int age)
+    { return age; }
+
+
+
+}
